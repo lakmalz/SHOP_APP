78aa369203afc721a75a53acc16324358d33db2c
Lakmal Weerasekara <alrweerasekara@gmail.com>
2021-03-20 23:44:51 +0530

    init project

---
+++ shop_app/lib/screens/home/body.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+      ],
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  Categories({Key key}) : super(key: key);
+
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(
+            categories[index],
+          );
+        },
+      ),
+    );
+  }
+}
